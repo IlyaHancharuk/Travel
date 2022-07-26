e39bc7938597b1c5ae30ef3988754cd66b6475e8
Ilya <innot707@gmail.com>
2022-07-26 16:22:31 +0300

    feat: add js-function for pop up

--- travel/script.js
+++ travel/script.js
@@ -53,19 +53,52 @@ let openModalBtn = document.querySelector('.btn-login')
 let openModalLink = document.getElementById('account')
 let modal = document.querySelector('.modal')
 let modal_body = document.querySelector('.modal-body')
+let modal_body_reg = document.querySelector('.modal-body-reg')
+let reg_link = document.querySelector('.reg-link')
+let login_link = document.querySelector('.login-link')
+let input_email_login = document.querySelector('.email-login')
+let input_pass_login = document.querySelector('.pass-login')
+let input_email_reg = document.querySelector('.email-reg')
+let input_pass_reg = document.querySelector('.pass-reg')
+
 
 function openPopUp() { 
     modal_body.style.top = '22.5%'
     modal.style.display = 'flex'
+    body.classList.toggle('no-scroll-page')
 }
 
 openModalBtn.addEventListener('click', openPopUp)
 
 openModalLink.addEventListener('click', openPopUp)
 
-window.addEventListener('click', function(event) {
+window.addEventListener('click', function(event) { 
     if (event.target === modal) {
         modal.style.display = 'none'
+        modal_body_reg.style.display = 'none'
+        modal_body.style.display = 'flex'
+        body.classList.toggle('no-scroll-page')
     }
 })
 
+reg_link.addEventListener('click', function() {
+    modal_body.style.display = 'none'
+    modal_body_reg.style.display = 'flex'
+})
+
+login_link.addEventListener('click', function() {
+    modal_body_reg.style.display = 'none'
+    modal_body.style.display = 'flex'
+})
+
+document.querySelector('.sign-in-site').addEventListener('click', function() {
+    if (input_email_login.value || input_pass_login.value) {
+        alert (`E-mail: ${input_email_login.value}\nPassword: ${input_pass_login.value}`)
+    }
+})
+
+document.querySelector('.sign-up-site').addEventListener('click', function() {
+    if (input_email_reg.value || input_pass_reg.value) {
+        alert (`E-mail: ${input_email_reg.value}\nPassword: ${input_pass_reg.value}`)
+    }
+})
